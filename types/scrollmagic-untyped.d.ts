e4ba4b59bf8e0ec87aa05e8a0e037612a009563c
jatin074 <jaytiwariffour4@gmail.com>
2025-04-08 08:04:34 +0530

    scrollmagic error resolved

--- types/scrollmagic-untyped.d.ts
+++ types/scrollmagic-untyped.d.ts
@@ -1,26 +1,26 @@
-declare module 'scrollmagic/scrollmagic/uncompressed/ScrollMagic' {
-    export class Controller {
-      destroy(reset?: boolean): void;
-    }
+// declare module 'scrollmagic/scrollmagic/uncompressed/ScrollMagic' {
+//     export class Controller {
+//       destroy(reset?: boolean): void;
+//     }
   
-    export interface SceneOptions {
-      triggerElement: Element | null;
-      duration?: number;
-      triggerHook?: number;
-    }
+//     export interface SceneOptions {
+//       triggerElement: Element | null;
+//       duration?: number;
+//       triggerHook?: number;
+//     }
   
-    export class Scene {
-      constructor(options: SceneOptions);
+//     export class Scene {
+//       constructor(options: SceneOptions);
   
-      setClassToggle(element: Element | null, className: string): this;
-      addTo(controller: Controller): this;
-    }
+//       setClassToggle(element: Element | null, className: string): this;
+//       addTo(controller: Controller): this;
+//     }
   
-    const ScrollMagic: {
-      Controller: typeof Controller;
-      Scene: typeof Scene;
-    };
+//     const ScrollMagic: {
+//       Controller: typeof Controller;
+//       Scene: typeof Scene;
+//     };
   
-    export default ScrollMagic;
-  }
+//     export default ScrollMagic;
+//   }
   
