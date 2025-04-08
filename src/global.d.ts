2b84e05dc0dbea02b954ff2ba664c01fe8ab9267
jatin074 <jaytiwariffour4@gmail.com>
2025-04-08 07:53:39 +0530

    resolved error

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,15 +1,1 @@
-declare module '*.css';
-declare module 'owl.carousel/dist/assets/owl.carousel.css';
-declare module 'owl.carousel/dist/assets/owl.theme.default.css';
 
-declare module 'owl.carousel' {
-  const OwlCarousel: any;
-  export default OwlCarousel;
-}
-
-declare global {
-  interface Window {
-    jQuery: any;
-    $: any;
-  }
-}
