5dc4edc88af9321a83cf10ff2fa0a7713ee4ccbf
jatin074 <jaytiwariffour4@gmail.com>
2025-06-06 08:47:21 +0530

    work changes

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -36,7 +36,7 @@ export default function Work() {
                 >
                     <SwiperSlide>
                         <div className="flex items-center justify-center w-full slide-items">
-                            <Image width={800} height={500} src="/images/work/work1.jpg" alt="Slide 1" />
+                            <Image width={800} height={500} src="/images/work/work6.png" alt="Slide 1" />
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
@@ -69,11 +69,82 @@ export default function Work() {
                             <Image width={800} height={500} src="/images/work/work7.png" alt="Slide 1" />
                         </div>
                     </SwiperSlide>
-                      <SwiperSlide>
+                    <SwiperSlide>
                         <div className="flex items-center justify-center w-full slide-items">
                             <Image width={800} height={500} src="/images/work/work8.png" alt="Slide 1" />
                         </div>
                     </SwiperSlide>
+                </Swiper>
+            </section>
+
+            <section className="lg:mt-[80px] mt-[60px]">
+                <Swiper
+                    loop={true}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    spaceBetween={50}
+                    autoplay={{ delay: 2000, reverseDirection: true, disableOnInteraction: false }}
+                    navigation={false}
+                    dir="rtl"
+                    modules={[Autoplay, Navigation]}
+                    breakpoints={{
+                        0: { slidesPerView: 1 },
+                        600: { slidesPerView: 2 },
+                        991: { slidesPerView: 2.5 },
+                        1200: { slidesPerView: 3 }
+                    }}
+                >
+                    <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work9.png" alt="Slide 9" />
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work10.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work11.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work12.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work13.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work14.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work15.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work16.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work17.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="flex items-center justify-center w-full slide-items">
+                            <Image width={800} height={500} src="/images/work/work18.png" alt="Slide 10" />
+                        </div>
+                    </SwiperSlide>
+                    
                 </Swiper>
             </section>
 
